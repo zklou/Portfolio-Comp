b6307e63cba7950ef02ca7e1ace287f018b9ac85
Zhengkun Lou <41825013+zklou@users.noreply.github.com>
2024-10-18 11:09:57 -0400

    nojekyll

--- next.config.js
+++ next.config.js
@@ -6,10 +6,10 @@ const nextConfig = {
 
 // next.config.js
 module.exports = {
-  output: 'export', // 生成静态导出
-  basePath: '/portfolio', // 使用 GitHub Pages 子路径
-  assetPrefix: '/portfolio', // 静态资源前缀
+  output: 'export', // 使用静态导出
+  basePath: '/Portfolio-Comp', // 项目在 GitHub Pages 上的子路径
+  assetPrefix: '/Portfolio-Comp', // 确保静态资源路径前缀与 basePath 一致
   images: {
-    unoptimized: true, // 禁用图像优化
+    unoptimized: true, // 禁用图像优化，因为 GitHub Pages 不支持动态优化
   },
 };
