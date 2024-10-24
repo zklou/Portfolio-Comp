b19c60144bd67eb343818d524eeeca3440b48335
Zhengkun Lou <41825013+zklou@users.noreply.github.com>
2024-10-24 15:49:39 -0400

    deploy

--- next.config.js
+++ next.config.js
@@ -4,13 +4,26 @@ const nextConfig = {
   swcMinify: true,
 }
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // next.config.js
+
 module.exports = {
-  output: 'export', // 使用静态导出
-  basePath: '/Portfolio-Comp',
-  //basePath: '/Portfolio-Comp', // 项目在 GitHub Pages 上的子路径
-  //assetPrefix: '/Portfolio-Comp', // 确保静态资源路径前缀与 basePath 一致
+  assetPrefix: isProd ? '/Portfolio-Comp/' : '',
+  basePath: isProd ? '/Portfolio-Comp' : '',
+  output: 'export', // 启用静态导出
+  trailingSlash: true, // 确保每个页面生成一个目录
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' }, // 对应 `index.jsx` (主页)
+      '/404': { page: '/404' }, // 对应 `404.jsx` (404 页面)
+      '/osc': { page: '/osc' }, // 对应 `osc.jsx`
+      // 如果有其他页面，可以在这里添加
+    };
+  },
   images: {
     unoptimized: true, // 禁用图像优化，因为 GitHub Pages 不支持动态优化
   },
 };
+
+
